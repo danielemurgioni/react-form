790708529ec268d3bd622037e4fa64314ba48c68
danielemurgioni <dashomew@gmail.com>
2025-05-09 15:43:19 +0200

    [UPD] create array

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+const games = [
+  { id: 1, title: "The Battle for Wesnoth" },
+  { id: 2, title: "Halo 3" },
+  { id: 3, title: "Terraria" },
+]
+
 function App() {
   return <h1>react form</h1>
 }
